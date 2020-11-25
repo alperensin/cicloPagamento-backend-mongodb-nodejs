0055c5541431f60b3e61419ff98ee2aefd994ae0
alperensin <andreluizperensin@hotmail.com>
2020-11-24 22:47:43 -0300

    FirstCommit - Setup Project

---
+++ config/server.js
@@ -0,0 +1,18 @@
+const port = 3003;
+
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+server.listen(port, () => {
+  console.log(`🔥 BackEnd is running on: http://localhost:${port}`);
+}); 
+
+server.use((req, res, next) => {
+  console.log('✔️ Funcionou!!');
+  res.send('✔️ Funcionou!!');
+})
